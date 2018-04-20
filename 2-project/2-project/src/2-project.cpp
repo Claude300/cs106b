2a2db6ae2283b9bdb503b89212b76beb9a5df8fb
Claude300 <maurice3000@me.com>
2018-04-20 05:04:27 -0300

    initial commit

---
+++ 2-project/2-project/src/2-project.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Type a integer" << endl;
+    int number = 0;
+    cin >> number;
+    cout << "You type the integer " << number << "." << endl;
+    return 0;
+}
